69417de289431848fe182565cb86bfaa5a0b8868
FordyDudeGuy <30471318+FordyDudeGuy@users.noreply.github.com>
2021-09-16 14:22:37 +0100

    Update modron.py

--- modron.py
+++ modron.py
@@ -16,7 +16,7 @@ async def on_ready():
 
 @bot.command(pass_context=True) 
 async def info(ctx):
-    await ctx.send('====================================\nHi'+ ctx.message.author.mention + '\n\nI am a Modron.\nMy sole purpose is to roll dice for you.\nPlease find a list of my commands below.\n_Commands_\n/r - Rolls a d20.\n/a - Rolls 2d20 for adv./disadv.\n/d X*d*Y* - for damage rolls.\n     where*X* = no.of dice and *Y* = dice sides\n====================================')    #await ctx.message.delete() 
+    await ctx.send('====================================\nHi'+ ctx.message.author.mention + '\n\nI am a Modron.\nMy sole purpose is to roll dice for you.\nPlease find a list of my commands below.\n_Commands_\n/r - Rolls a d20.\n/a - Rolls 2d20 for adv./disadv.\n/d X*d*Y* - for damage rolls.\n     where*X* = no.of dice and *Y* = dice sides\n*/b to add a break for clarity.\n*/flip* - Flip the virtual table.\n====================================')    #await ctx.message.delete() 
 
 @bot.command(pass_context=True) 
 async def b(ctx):
