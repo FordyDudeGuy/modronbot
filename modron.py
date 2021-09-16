05b981d318994a1deed064bf802b8acde2889ab2
Yopparai <30471318+FordyDudeGuy@users.noreply.github.com>
2021-09-16 16:28:54 +0000

    Flip Error message tweak

--- modron.py
+++ modron.py
@@ -51,7 +51,7 @@ async def flip(ctx):
 @flip.error
 async def command_name_error(ctx, error):
         if isinstance(error, commands.CommandOnCooldown):
-            await ctx.send("Sorry %s, I still haven't unflipped the table from last time you asked me to flip it." % ctx.message.author.name )
+            await ctx.send("Sorry %s, I'm still unflipping the table from last time you asked me to flip it. Please try again in a moment." % ctx.message.author.name )
  
 @bot.command(pass_context=True)
 async def d(ctx, roll : str):
