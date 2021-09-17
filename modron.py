68a7f4c77fe38d7cfa2b63a5d42ada7271b57998
Yopparai <30471318+FordyDudeGuy@users.noreply.github.com>
2021-09-17 15:18:39 +0000

    output message

--- modron.py
+++ modron.py
@@ -162,8 +162,8 @@ async def r(ctx, *roll,):
     # Output: If the number of dice was more than 1 
     else:
         grandTotal = resultTotal + rollModifier
-        await ctx.send("====================================\nRolling *%sd%s* for %s" % (numDice, diceVal, ctx.message.author.mention)) 
-        await ctx.send("\n**Result:**" + resultString + "\n*Total:*" + str(grandTotal) + '\n *Total*' + "\n====================================")
+        await ctx.send("====================================\nRolling *%sd%s* for %s" % (numDice, diceVal, ctx.message.author.mention) + "\n**Result:** " + resultString + "\n*Subtotal:* " + str(resultTotal) + '+' + '\n *Total:* ' + "*"+ grandTotal + "*"+"\n====================================") 
+        
         return
          
       #Trying to roll more than 100 dice.
