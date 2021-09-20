cb82d2b69b7246d607e2bec4a996f4cf2c65f09f
Yopparai <30471318+FordyDudeGuy@users.noreply.github.com>
2021-09-20 15:31:38 +0000

    made output strs

--- modron.py
+++ modron.py
@@ -68,7 +68,7 @@ async def adv(ctx, *modifier,):
       if positiveModifierNumber.isnumeric():
         result1 = int(result_list[0]) + int(positiveModifierNumber)
         result2 = int(result_list[1]) + int(positiveModifierNumber)
-        await ctx.send('====================================\nRolling two d20s for ' + ctx.message.author.mention + "Dice Rolls: " + result_list[0] +' and '+ result_list[1] + "\n*Totals:* **" + str(result1) + '** and **' +  str(result2) + "**\n====================================")
+        await ctx.send('====================================\nRolling two d20s for ' + ctx.message.author.mention + "Dice Rolls: " + str(result_list[0]) +' and '+ (result_list[1]) + "\n*Totals:* **" + str(result1) + '** and **' +  str(result2) + "**\n====================================")
         return
     
 
