473bfbb80583125c7a5a8b8b1e3bb269708d745f
Yopparai <30471318+FordyDudeGuy@users.noreply.github.com>
2021-09-21 12:31:56 +0000

    other roll types now have mods both postive and negatives

--- modron.py
+++ modron.py
@@ -21,41 +21,157 @@ async def info(ctx):
 
 # Roll a d4
 @bot.command(pass_context=True, aliases=['D4']) 
-async def d4(ctx):
-    await ctx.send('====================================\nRolling a d4 for ' + ctx.message.author.mention + "  *Result:* " + '**' +  str(random.randint(1, 4))+ '**' +  "\n====================================")
-
-# Roll a d6
-@bot.command(pass_context=True, aliases=['D6']) 
-async def d6(ctx):
-    await ctx.send('====================================\nRolling a d6 for ' + ctx.message.author.mention + "  *Result:* " + '**' +  str(random.randint(1, 6))+ '**' +  "\n====================================")
-
-# Roll a d8
-@bot.command(pass_context=True, aliases=['D8']) 
-async def d8(ctx):
-    await ctx.send('====================================\nRolling a d8 for ' + ctx.message.author.mention + "  *Result:* " + '**' +  str(random.randint(1, 8))+ '**' +  "\n====================================")
-
-# Roll a d10
-@bot.command(pass_context=True, aliases=['D10']) 
-async def d10(ctx):
-    await ctx.send('====================================\nRolling a d10 for ' + ctx.message.author.mention + "  *Result:* " + '**' +  str(random.randint(1, 10))+ '**' +  "\n====================================")
-
-# Roll a d12
-@bot.command(pass_context=True, aliases=['D12']) 
-async def d12(ctx):
-    await ctx.send('====================================\nRolling a d12 for ' + ctx.message.author.mention + "  *Result:* " + '**' +  str(random.randint(1, 12))+ '**' +  "\n====================================") 
-
-# Roll a d20
-@bot.command(pass_context=True, aliases=['D20']) 
-async def d20(ctx, *modifier):
+async def d4(ctx, *modifier):
   joinedModifier = ''.join(modifier)
+  roll = random.randint(1, 4)
   if not modifier:
-    await ctx.send('====================================\nRolling a d20 for ' + ctx.message.author.mention + "  *Result:* " + '**' + str(random.randint(1, 20))+ '**' +  "\n====================================")
+    await ctx.send('====================================\nRolling a d4 for ' + ctx.message.author.mention + "  *Result:* " + '**' + str(roll) + '**' +  "\n====================================")
     return
 
   elif joinedModifier.find('+') != -1:
       positiveModifierNumber = str(joinedModifier.replace('+',''))
       if positiveModifierNumber.isnumeric():
-         roll= random.randint(1, 20)
+         total = roll + int(positiveModifierNumber)
+         await ctx.send('====================================\nRolling a d4 and adding ' + str(positiveModifierNumber) + ' for ' + ctx.message.author.mention + " \n*Roll:* " + str(roll) + "\n*Result:* **" + str(total) + "**\n====================================")
+         return
+      else: 
+        await ctx.send("I'm sorry, I didn't understand the term " + joinedModifier + ". Please try again.")
+  
+  elif joinedModifier.find('-') != -1:
+      negativeModifierNumber = str(joinedModifier.replace('-',''))
+      if negativeModifierNumber.isnumeric():
+         
+         total = roll - int(negativeModifierNumber)
+         await ctx.send('====================================\nRolling a d4 and subtracting ' + str(negativeModifierNumber) + ' for ' + ctx.message.author.mention + " \n*Roll:* " + str(roll) + "\n*Result:* **" + str(total) + "**\n====================================")
+         return
+      else: 
+        await ctx.send("I'm sorry, I didn't understand the term " + joinedModifier + ". Please try again.")
+
+
+# Roll a d6
+@bot.command(pass_context=True, aliases=['D6']) 
+async def d6(ctx, *modifier):
+  joinedModifier = ''.join(modifier)
+  roll = random.randint(1, 6)
+  if not modifier:
+    await ctx.send('====================================\nRolling a d6 for ' + ctx.message.author.mention + "  *Result:* " + '**' + str(roll) + '**' +  "\n====================================")
+    return
+
+  elif joinedModifier.find('+') != -1:
+      positiveModifierNumber = str(joinedModifier.replace('+',''))
+      if positiveModifierNumber.isnumeric():
+         total = roll + int(positiveModifierNumber)
+         await ctx.send('====================================\nRolling a d6 and adding ' + str(positiveModifierNumber) + ' for ' + ctx.message.author.mention + " \n*Roll:* " + str(roll) + "\n*Result:* **" + str(total) + "**\n====================================")
+         return
+      else: 
+        await ctx.send("I'm sorry, I didn't understand the term " + joinedModifier + ". Please try again.")
+  
+  elif joinedModifier.find('-') != -1:
+      negativeModifierNumber = str(joinedModifier.replace('-',''))
+      if negativeModifierNumber.isnumeric():
+         
+         total = roll - int(negativeModifierNumber)
+         await ctx.send('====================================\nRolling a d6 and subtracting ' + str(negativeModifierNumber) + ' for ' + ctx.message.author.mention + " \n*Roll:* " + str(roll) + "\n*Result:* **" + str(total) + "**\n====================================")
+         return
+      else: 
+        await ctx.send("I'm sorry, I didn't understand the term " + joinedModifier + ". Please try again.")
+
+# Roll a d8
+@bot.command(pass_context=True, aliases=['D8']) 
+async def d8(ctx, *modifier):
+  joinedModifier = ''.join(modifier)
+  roll = random.randint(1, 8)
+  if not modifier:
+    await ctx.send('====================================\nRolling a d8 for ' + ctx.message.author.mention + "  *Result:* " + '**' + str(roll) + '**' +  "\n====================================")
+    return
+
+  elif joinedModifier.find('+') != -1:
+      positiveModifierNumber = str(joinedModifier.replace('+',''))
+      if positiveModifierNumber.isnumeric():
+         total = roll + int(positiveModifierNumber)
+         await ctx.send('====================================\nRolling a d8 and adding ' + str(positiveModifierNumber) + ' for ' + ctx.message.author.mention + " \n*Roll:* " + str(roll) + "\n*Result:* **" + str(total) + "**\n====================================")
+         return
+      else: 
+        await ctx.send("I'm sorry, I didn't understand the term " + joinedModifier + ". Please try again.")
+  
+  elif joinedModifier.find('-') != -1:
+      negativeModifierNumber = str(joinedModifier.replace('-',''))
+      if negativeModifierNumber.isnumeric():
+         
+         total = roll - int(negativeModifierNumber)
+         await ctx.send('====================================\nRolling a d8 and subtracting ' + str(negativeModifierNumber) + ' for ' + ctx.message.author.mention + " \n*Roll:* " + str(roll) + "\n*Result:* **" + str(total) + "**\n====================================")
+         return
+      else: 
+        await ctx.send("I'm sorry, I didn't understand the term " + joinedModifier + ". Please try again.")
+
+# Roll a d10
+@bot.command(pass_context=True, aliases=['D10']) 
+async def d10(ctx, *modifier):
+  joinedModifier = ''.join(modifier)
+  roll = random.randint(1, 10)
+  if not modifier:
+    await ctx.send('====================================\nRolling a d10 for ' + ctx.message.author.mention + "  *Result:* " + '**' + str(roll) + '**' +  "\n====================================")
+    return
+
+  elif joinedModifier.find('+') != -1:
+      positiveModifierNumber = str(joinedModifier.replace('+',''))
+      if positiveModifierNumber.isnumeric():
+         total = roll + int(positiveModifierNumber)
+         await ctx.send('====================================\nRolling a d10 and adding ' + str(positiveModifierNumber) + ' for ' + ctx.message.author.mention + " \n*Roll:* " + str(roll) + "\n*Result:* **" + str(total) + "**\n====================================")
+         return
+      else: 
+        await ctx.send("I'm sorry, I didn't understand the term " + joinedModifier + ". Please try again.")
+  
+  elif joinedModifier.find('-') != -1:
+      negativeModifierNumber = str(joinedModifier.replace('-',''))
+      if negativeModifierNumber.isnumeric():
+         
+         total = roll - int(negativeModifierNumber)
+         await ctx.send('====================================\nRolling a d10 and subtracting ' + str(negativeModifierNumber) + ' for ' + ctx.message.author.mention + " \n*Roll:* " + str(roll) + "\n*Result:* **" + str(total) + "**\n====================================")
+         return
+      else: 
+        await ctx.send("I'm sorry, I didn't understand the term " + joinedModifier + ". Please try again.")
+
+# Roll a d12
+@bot.command(pass_context=True, aliases=['D12']) 
+async def d12(ctx, *modifier):
+  joinedModifier = ''.join(modifier)
+  roll = random.randint(1, 12)
+  if not modifier:
+    await ctx.send('====================================\nRolling a d12 for ' + ctx.message.author.mention + "  *Result:* " + '**' + str(roll) + '**' +  "\n====================================")
+    return
+
+  elif joinedModifier.find('+') != -1:
+      positiveModifierNumber = str(joinedModifier.replace('+',''))
+      if positiveModifierNumber.isnumeric():
+         total = roll + int(positiveModifierNumber)
+         await ctx.send('====================================\nRolling a d12 and adding ' + str(positiveModifierNumber) + ' for ' + ctx.message.author.mention + " \n*Roll:* " + str(roll) + "\n*Result:* **" + str(total) + "**\n====================================")
+         return
+      else: 
+        await ctx.send("I'm sorry, I didn't understand the term " + joinedModifier + ". Please try again.")
+  
+  elif joinedModifier.find('-') != -1:
+      negativeModifierNumber = str(joinedModifier.replace('-',''))
+      if negativeModifierNumber.isnumeric():
+         
+         total = roll - int(negativeModifierNumber)
+         await ctx.send('====================================\nRolling a d12 and subtracting ' + str(negativeModifierNumber) + ' for ' + ctx.message.author.mention + " \n*Roll:* " + str(roll) + "\n*Result:* **" + str(total) + "**\n====================================")
+         return
+      else: 
+        await ctx.send("I'm sorry, I didn't understand the term " + joinedModifier + ". Please try again.")
+
+# Roll a d20
+@bot.command(pass_context=True, aliases=['D20']) 
+async def d20(ctx, *modifier):
+  joinedModifier = ''.join(modifier)
+  roll = random.randint(1, 20)
+  if not modifier:
+    await ctx.send('====================================\nRolling a d20 for ' + ctx.message.author.mention + "  *Result:* " + '**' + str(roll) + '**' +  "\n====================================")
+    return
+
+  elif joinedModifier.find('+') != -1:
+      positiveModifierNumber = str(joinedModifier.replace('+',''))
+      if positiveModifierNumber.isnumeric():
          total = roll + int(positiveModifierNumber)
          await ctx.send('====================================\nRolling a d20 and adding ' + str(positiveModifierNumber) + ' for ' + ctx.message.author.mention + " \n*Roll:* " + str(roll) + "\n*Result:* **" + str(total) + "**\n====================================")
          return
@@ -65,7 +181,7 @@ async def d20(ctx, *modifier):
   elif joinedModifier.find('-') != -1:
       negativeModifierNumber = str(joinedModifier.replace('-',''))
       if negativeModifierNumber.isnumeric():
-         roll= random.randint(1, 20)
+         
          total = roll - int(negativeModifierNumber)
          await ctx.send('====================================\nRolling a d20 and subtracting ' + str(negativeModifierNumber) + ' for ' + ctx.message.author.mention + " \n*Roll:* " + str(roll) + "\n*Result:* **" + str(total) + "**\n====================================")
          return
