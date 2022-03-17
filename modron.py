641166b8733321ff9309a4af4ea9012108997831
Yopparai <30471318+FordyDudeGuy@users.noreply.github.com>
2022-03-17 22:52:48 +0000

    remove print strings and cleanup code. fingers crossed doesn't break anything

--- modron.py
+++ modron.py
@@ -1,12 +1,10 @@
-# Import requirements.
+# Import requirements and initialise bot prefix and get token from secret value.
 import discord
 import os
 import random
 from discord.ext import commands
 from discord.ext.commands import Bot
 import re
-
-#Initialise bot prefix and get token from secret value.
 bot = Bot(command_prefix='/')
 Bot_Token = os.environ['TOKEN']
 
@@ -14,11 +12,6 @@ Bot_Token = os.environ['TOKEN']
 @bot.event
 async def on_ready():
 	print(f'*** {bot.user} online. ***')
-
-# Info command. 
-@bot.command(pass_context=True, aliases=['i', 'I'])
-async def info(ctx):
-    await ctx.send('==========================\nHi there'+ ctx.message.author.mention +'!'+ "\nI am a Modron.\nMy sole purpose is to roll dice for you.\nPlease find a list of my commands below.\n\n_Commands_\n\n/r *x*d*y* where *x* = no. of dice and *y* = dice sides\nIf you miss out *x*, I'll assume you just want the one. If you leave out *x*d*y*, I'll just  assume you just want a d20. You can also add a + for more dice to be rolled at the same time or for a modifier \n/adv (or just /a) - Rolls 2d20 for adv./disadv sorted in asc. order.\n==========================")    
 
 #statgen
 @bot.command(pass_context=True, aliases=['stats']) 
@@ -37,18 +30,16 @@ async def statgen(ctx):
     await ctx.send('==========================\nRolling stats for ' + ctx.message.author.mention + "\n*Results:* **" + str(statList[0]) + ' ' + str(statList[1]) + ' ' +str(statList[2]) + ' ' +str(statList[3]) + ' ' + str(statList[4]) + ' ' + str(statList[5]) + "**\n==========================")  
     return 
     
-#Roll
+#roll command
 @bot.command(pass_context=True, aliases=['roll', 'ROLL', 'Roll'])
 async def r(ctx, *roll,):
-#/r command takes a string argument that will be processed and used later.
-  
-#if no argument is given then just roll a d20.
+ 
+  #if no argument is given then just roll a d20.
     if not roll:
       await ctx.send('==========================\n*Rolling a d20 for ' + ctx.message.author.mention + "*\n**Dice Roll: " + str(random.randint(1, 20))+'**'+"\n==========================")
       return
-      
 
-#initialise variables
+  #initialise variables
     resultTotal = 0
     resultNTotal = 0
     resultString = ''
@@ -59,12 +50,12 @@ async def r(ctx, *roll,):
     positiveList = []
     negativeList= []
 
-# This converts the argument (which is a Tuple) to a string with no spaces. Then separates the string into a list of individual terms that were separated by a + or a -. 
+  # This converts the argument (which is a Tuple) to a string with no spaces. Then separates the string into a list of individual terms that were separated by a + or a -. 
       
     joinedRoll= ''.join(roll)  
     rollList = re.findall('[-+]?\w+', joinedRoll.replace(' ', ''))
     
-# While loop that splits terms into positive list and negative list.
+  # While loop that splits terms into positive list and negative list.
     
     while i < len(rollList):
       if "-" in str(rollList[i]):
@@ -73,8 +64,7 @@ async def r(ctx, *roll,):
         positiveList.append(rollList[i])
       i = i + 1 
     
-    
-#Positive term resolution
+  #Positive term resolution
     i = 0
     while i < len(positiveList):
       positiveList[i] = positiveList[i].replace ("+", "")
@@ -86,11 +76,11 @@ async def r(ctx, *roll,):
           numDice = positiveList[i].split('d')[0]
           diceVal = positiveList[i].split('d')[1]
 
-    # If the number of dice is not specified default to one dice of given type. 
+  # If the number of dice is not specified default to one dice of given type. 
           if str(numDice) =='':
             numDice = int(1)
 
-    #reset n then do another while loop to create results string.
+  #reset n then do another while loop to create results string.
           n = 0
           while n < int(numDice):
             diceResult = random.randint(1, int(diceVal))
@@ -109,11 +99,10 @@ async def r(ctx, *roll,):
             return
         i = i + 1
          
-#negative term resolution
+  #negative term resolution
     i = 0
     while i < len(negativeList):
       negativeList[i] = negativeList[i].replace("-","")
-      print ( "this is the negative list:",negativeList)
       if "d" not in negativeList[i]:
         rollNModifier = rollNModifier + int(negativeList[i])
         i = i + 1
@@ -122,11 +111,11 @@ async def r(ctx, *roll,):
           numDice = negativeList[i].split('d')[0]
           diceVal = negativeList[i].split('d')[1]
 
-    # If the number of dice is not specified default to one dice of given type. 
+  # If the number of dice is not specified default to one dice of given type. 
           if str(numDice) =='':
             numDice = int(1)
             
-    #reset n then do another while loop to create results string.
+  #reset n then do another while loop to create results string.
           n = 0
           while n < int(numDice):
             diceResult = random.randint(1, int(diceVal))
@@ -135,11 +124,9 @@ async def r(ctx, *roll,):
             if resultString == '':
               resultString += str(diceResult)
               n = n + 1
-              print ("starting new string for negative string")                        
+                                     
             else:
               resultString += ', -' + str(diceResult)
-              print ("added dice result:")
-              print (diceResult)
               n = n + 1
           
         
@@ -147,10 +134,8 @@ async def r(ctx, *roll,):
             await ctx.send("Error. I didn't understand that command %s." % (ctx.message.author.mention))
             return
       i = i + 1
-         
       
-      
-  # # Output
+  # Output
     print (resultNTotal)
     grandTotal = resultTotal + rollModifier  - rollNModifier - resultNTotal
     printedRoll= joinedRoll.replace("+", " + ")
@@ -159,11 +144,10 @@ async def r(ctx, *roll,):
     await ctx.send("==========================\n*Rolling: " + printedRoll + "*  for %s" % (ctx.message.author.mention) + "\nDice Rolls: " + resultString + "\n**Total:**  " + "**" + str(grandTotal) + "**" + "\n==========================")
     return
                    
-# Adv./ Disadv.
+#adv./disadv. command
 @bot.command(pass_context=True, aliases=['a','A']) 
 async def adv(ctx, *modifier,):
-    result_list = [random.randint(1,20) for _ in range(2)]
-    #result_list.sort()
+    result_list = [random.randint(1,20) for _ in range(2)] 
     joinedModifier = ''.join(modifier)
     
     if not modifier:
