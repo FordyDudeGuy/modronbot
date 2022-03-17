c6585170341c68c8925c8093e93e58ed1263b01f
Yopparai <30471318+FordyDudeGuy@users.noreply.github.com>
2022-03-17 16:50:46 +0000

    formatting output negativerollmod

--- modron.py
+++ modron.py
@@ -148,6 +148,7 @@ async def r(ctx, *roll,):
     grandTotal = resultTotal + rollModifier  + rollNModifier
     printedRoll= joinedRoll.replace("+", " + ")
     printedRoll= printedRoll.replace("-", " - ")
+    rollNModifier = printedRoll.replace("-", "")
     await ctx.send("====================================\nRolling *" + printedRoll + "*  for %s" % (ctx.message.author.mention) + "\n*Result:* " + resultString + "\n*Subtotal:* " + str(resultTotal) + ' + ' + str(rollModifier) + " - " + str(rollNModifier) + '\n*Total:*  ' + "**" + str(grandTotal) + "**"+"\n====================================")
     return
     #     else:
