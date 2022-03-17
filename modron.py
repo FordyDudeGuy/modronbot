c1914e8c0b2b54df8e00e88392cb5f20f6d65fcc
Yopparai <30471318+FordyDudeGuy@users.noreply.github.com>
2022-03-17 22:15:22 +0000

    remove print strings and other cleanup

--- modron.py
+++ modron.py
@@ -1,12 +1,10 @@
-# Import requirements.
+# Import requirements and initialise bot prefix and get token from secret value.
 import discord
 import os
 import random
 from discord.ext import commands
 from discord.ext.commands import Bot
 import re
-
-#Initialise bot prefix and get token from secret value.
 bot = Bot(command_prefix='/')
 Bot_Token = os.environ['TOKEN']
 
@@ -18,7 +16,7 @@ async def on_ready():
 # Info command. 
 @bot.command(pass_context=True, aliases=['i', 'I'])
 async def info(ctx):
-    await ctx.send('==========================\nHi there'+ ctx.message.author.mention +'!'+ "\nI am a Modron.\nMy sole purpose is to roll dice for you.\nPlease find a list of my commands below.\n\n_Commands_\n\n/r *x*d*y* where *x* = no. of dice and *y* = dice sides\nIf you miss out *x*, I'll assume you just want the one. If you leave out *x*d*y*, I'll just  assume you just want a d20. You can also add a + for more dice to be rolled at the same time or for a modifier \n/adv (or just /a) - Rolls 2d20 for adv./disadv sorted in asc. order.\n==========================")    
+    await ctx.send('==========================\nHi there'+ ctx.message.author.mention +'!'+ "\nI am a Modron.\nMy sole purpose is to roll dice for you.\n_Commands_\n\n/r *x*d*y* where *x* = no. of dice and *y* = dice sides\nYou can also add a + for more dice to be rolled at the same time or for a modifier \n/adv (or just /a) - Rolls 2d20 for adv./disadv, you can also add a modifier to this too.\n/statgen - I'll roll you six attributes ususing 4d6 drop the lowest method.==========================")    
 
 #statgen
 @bot.command(pass_context=True, aliases=['stats']) 
@@ -37,18 +35,15 @@ async def statgen(ctx):
     await ctx.send('==========================\nRolling stats for ' + ctx.message.author.mention + "\n*Results:* **" + str(statList[0]) + ' ' + str(statList[1]) + ' ' +str(statList[2]) + ' ' +str(statList[3]) + ' ' + str(statList[4]) + ' ' + str(statList[5]) + "**\n==========================")  
     return 
     
-#Roll
+#Roll command
 @bot.command(pass_context=True, aliases=['roll', 'ROLL', 'Roll'])
 async def r(ctx, *roll,):
-#/r command takes a string argument that will be processed and used later.
-  
-#if no argument is given then just roll a d20.
+  #if no argument is given then just roll a d20.
     if not roll:
       await ctx.send('==========================\nRolling a d20 for ' + ctx.message.author.mention + "  *Result:* " + '**'+str(random.randint(1, 20))+'**'+"\n==========================")
       return
       
-
-#initialise variables
+  #initialise variables
     resultTotal = 0
     resultNTotal = 0
     resultString = ''
@@ -59,12 +54,12 @@ async def r(ctx, *roll,):
     positiveList = []
     negativeList= []
 
-# This converts the argument (which is a Tuple) to a string with no spaces. Then separates the string into a list of individual terms that were separated by a + or a -. 
+  # This converts the argument (which is a Tuple) to a string with no spaces and separates the string into a list of individual terms that were separated by a + or a -. 
       
     joinedRoll= ''.join(roll)  
     rollList = re.findall('[-+]?\w+', joinedRoll.replace(' ', ''))
     
-# While loop that splits terms into positive list and negative list.
+  # While loop that splits terms into positive list and negative list.
     
     while i < len(rollList):
       if "-" in str(rollList[i]):
@@ -73,8 +68,7 @@ async def r(ctx, *roll,):
         positiveList.append(rollList[i])
       i = i + 1 
     
-    
-#Positive term resolution
+  #Positive term resolution
     i = 0
     while i < len(positiveList):
       positiveList[i] = positiveList[i].replace ("+", "")
@@ -108,7 +102,7 @@ async def r(ctx, *roll,):
             await ctx.send("Error. I didn't understand that command %s." % (ctx.message.author.mention))
             return
          
-#negative term resolution
+  #negative term resolution
     i = 0
     while i < len(negativeList):
       negativeList[i] = negativeList[i].replace("-","")
@@ -130,26 +124,19 @@ async def r(ctx, *roll,):
           while n < int(numDice):
             diceResult = random.randint(1, int(diceVal))
             resultNTotal = int(resultNTotal) + int(diceResult)
-            print (diceVal)
             if resultString == '':
               resultString += str(diceResult)
-              n = n + 1
-              print ("starting new string for negative string")                        
+              n = n + 1                       
             else:
               resultString += ', -' + str(diceResult)
-              print ("added dice result:")
-              print (diceResult)
               n = n + 1
           i = i + 1
         
         except Exception:      
             await ctx.send("Error. I didn't understand that command %s." % (ctx.message.author.mention))
             return
-         
       
-      
-  # # Output
-    print (resultNTotal)
+  # Output
     grandTotal = resultTotal + rollModifier  - rollNModifier - resultNTotal
     printedRoll= joinedRoll.replace("+", " + ")
     printedRoll= printedRoll.replace("-", " - ")
@@ -157,7 +144,7 @@ async def r(ctx, *roll,):
     await ctx.send("==========================\nRolling: *" + printedRoll + "*  for %s" % (ctx.message.author.mention) + "\nDice Rolls:* " + resultString + "*\n**Total:**  " + "**" + str(grandTotal) + "**" + "\n==========================")
     return
                    
-# Adv./ Disadv.
+# Adv./ Disadv command
 @bot.command(pass_context=True, aliases=['a','A']) 
 async def adv(ctx, *modifier,):
     result_list = [random.randint(1,20) for _ in range(2)]
