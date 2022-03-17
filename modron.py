ed38644e866cd1eb0d64cbef5bad2dff06e673f5
Yopparai <30471318+FordyDudeGuy@users.noreply.github.com>
2022-03-17 09:44:04 +0000

    w pattern

--- modron.py
+++ modron.py
@@ -63,7 +63,7 @@ async def r(ctx, *roll,):
     rollList = joinedRoll.split('+') 
     
     #New roll parser using re.findall
-    exList = re.findall('[-+]?\.+', joinedRoll.replace(' ', ''))
+    exList = re.findall('[-+]?\w+', joinedRoll.replace(' ', ''))
     print (exList)
       
        
